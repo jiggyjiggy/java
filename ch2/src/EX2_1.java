a5766587269641e7222f3ecdd749616e8bbb4e71
jiggy <jiggyjiggy0323@gmail.com>
2022-06-27 03:50:13 +0900

    ch2:1~2

---
+++ ch2/src/EX2_1.java
@@ -0,0 +1,19 @@
+public class EX2_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Hello");
+        System.out.print("Hello");
+        System.out.println("Hello");
+
+        System.out.println(3+5);
+        System.out.println(3-5);
+        System.out.println(3*5);
+        System.out.println(3/5);
+        System.out.println(3/5.0);
+    }
+}
+
+
+
+
